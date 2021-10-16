6b2321282bb1bc5061cf525359443048a6d72855
kevinplevi <kevinplevi@gmail.com>
2021-10-16 18:21:15 +0700

    add more function

--- attestationLibs/src/main/java/com/example/attestationlibs/Init.java
+++ attestationLibs/src/main/java/com/example/attestationlibs/Init.java
@@ -1,7 +1,15 @@
 package com.example.attestationlibs;
 
+import android.util.Log;
+
 public class Init {
     public void talk() {
         System.out.print("THIS FROM PUBLISH LIBRARY");
+    }
+    public void testlog() {
+        Log.i("libs", "testlog: THIS FROM LIBS");
+    }
+    public String word() {
+        return "RETURN STRING";
     }
 }
