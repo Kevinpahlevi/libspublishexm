3c1464d24996f79f83748eb10fa09a9945b2d781
kevinplevi <kevinplevi@gmail.com>
2021-10-16 18:50:39 +0700

    try add other depedencies

--- attestationLibs/src/main/java/com/example/attestationlibs/Init.java
+++ attestationLibs/src/main/java/com/example/attestationlibs/Init.java
@@ -1,6 +1,16 @@
 package com.example.attestationlibs;
 
+import android.content.Context;
 import android.util.Log;
+
+import com.fingerprintjs.android.fingerprint.Configuration;
+import com.fingerprintjs.android.fingerprint.FingerprintResult;
+import com.fingerprintjs.android.fingerprint.Fingerprinter;
+import com.fingerprintjs.android.fingerprint.FingerprinterFactory;
+import com.fingerprintjs.android.fingerprint.signal_providers.StabilityLevel;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class Init {
     public void talk() {
@@ -11,5 +21,18 @@ public class Init {
     }
     public String word() {
         return "RETURN STRING";
+    }
+    public void finger(Context context) {
+        Fingerprinter fingerprinter = FingerprinterFactory
+               .getInstance(context, new Configuration(3));
+
+        fingerprinter.getFingerprint(StabilityLevel.OPTIMAL, new Function1<FingerprintResult, Unit>() {
+            @Override
+            public Unit invoke(FingerprintResult fingerprintResult) {
+                String fingerprint = fingerprintResult.getFingerprint();
+                Log.d("libs", "FINGERPRINT ID FROM LIBS => " +fingerprint);
+                return null;
+            }
+        });
     }
 }
